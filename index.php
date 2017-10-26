0077d0cbc6835f9325464efc25590cc52b04c844
JonathanDonoso <jonadonoso22@gmail.com>
2017-10-26 19:16:47 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+       <link rel="stylesheet" type="text/css" href="estilos/stylocatalogo.css">  <!--catalogo-->
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">  <!--meta:vp-->
+     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+     
+     
+
+	<title>Catálogo del Hotel</title>
+</head>
+<body>
+        <b>Nuestra Hotelería</b>
+       <div id="contenedor-slider" class="contenedor-slider">
+        <div id="slider" class="slider">
+            
+            <section class="slider_section"> <img src="Fotos_hotel/img1.jpg" class="slider_img"></section>
+            <section class="slider_section"> <img src="Fotos_hotel/img2.jpg" class="slider_img"></section>
+            <section class="slider_section"> <img src="Fotos_hotel/img3.jpg" class="slider_img"></section>
+            <section class="slider_section"> <img src="Fotos_hotel/img4.jpg" class="slider_img"></section>
+            
+        </div>        
+           
+           <div id="btn-prev" class="btn-prev">&#60</div>
+            <div id="btn-next" class="btn-next">&#62</div>
+       </div>
+     
+   <script src="js/slider.js"></script>
+
+</body>
+</html>
